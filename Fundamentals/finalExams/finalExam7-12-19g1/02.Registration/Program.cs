896943de99a71968c829479b499e94c863b7e3de
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 20:27:09 +0200

    Add files via upload

---
+++ Fundamentals/finalExams/finalExam7-12-19g1/02.Registration/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.Registration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int howMany = int.Parse(Console.ReadLine());
+            int sumRegistration = 0;
+            for (int i = 0; i < howMany; i++)
+            {
+                string input = Console.ReadLine();
+                string pattern = @"U\$(?<username>[A-Z][a-z]{2,})U\$P@\$(?<password>[A-Za-z]{5,}\d+)P@\$";
+                Match regisstration = Regex.Match(input, pattern);
+                if (regisstration.Success)
+                {
+                    Console.WriteLine("Registration was successful");
+                    Console.WriteLine($"Username: {regisstration.Groups["username"].Value}, Password: {regisstration.Groups["password"].Value}");
+                    sumRegistration++;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username or password");
+                }
+            }
+            Console.WriteLine($"Successful registrations: {sumRegistration}");
+        }
+    }
+}
---
+++ Fundamentals/finalExams/finalExam7-12-19g1/emailValidator/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+
+namespace finalExam7_12_19g1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string email = Console.ReadLine();
+
+            string command = "";
+
+            while ((command = Console.ReadLine()) != "Complete")
+            {
+
+                string[] commArgs = command.Split().ToArray();
+
+                switch (commArgs[0])
+                {
+                    case "Make":
+
+                        if (commArgs[1] == "Upper")
+                        {
+                            email = email.ToUpper();
+                        }
+                        else if (commArgs[1] == "Lower")
+                        {
+                            email = email.ToLower();
+                        }
+                        Console.WriteLine(email);
+                        break;
+
+                    case "GetDomain":
+                        int count = int.Parse(commArgs[1]);
+
+                        if (count < email.Length && count >= 0)
+                        {
+                            Console.WriteLine(email.Substring(email.Length - count));
+                        }
+                        else
+                        {
+                            Console.WriteLine(email);
+                        }
+                        break;
+
+                    case "GetUsername":
+                        if (email.Contains('@'))
+                        {
+                            int indexOfAt = email.IndexOf('@');
+                            string username = email.Substring(0, indexOfAt);
+                            Console.WriteLine(username);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"The email {email} doesn't contain the @ symbol.");
+                        }
+                        break;
+
+                    case "Replace":
+                        char currentCh = char.Parse(commArgs[1]);
+                        email = email.Replace(currentCh, '-');
+                        Console.WriteLine(email);
+                        break;
+
+                    case "Encrypt":
+                        for (int i = 0; i < email.Length; i++)
+                        {
+                            char currSumbol = email[i];
+                            Console.Write((int)currSumbol + " ");
+                        }
+                        Console.WriteLine();
+                        break;
+                }
+
+            }
+
+        }
+    }
+}
