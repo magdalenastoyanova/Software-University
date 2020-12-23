a07f9848c97933050a9dd7ff6f1c0cbc06d49930
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 18:33:46 +0200

    Add files via upload

---
+++ Fundamentals/ArraysEx/topInteger/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace topInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arr = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            string result = "";
+           
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int current = arr[i];
+                bool isTopInteger = true;
+
+                for (int index = i + 1; index < arr.Length; index++)
+                {
+                    if (current <= arr[index])
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                }
+                if (isTopInteger)
+                {
+                    result += current + " ";
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
