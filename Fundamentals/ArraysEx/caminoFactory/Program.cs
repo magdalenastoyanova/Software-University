90ceab6dfa51dd6e525143f50d6faa01f8b5c806
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 18:36:56 +0200

    Add files via upload

---
+++ Fundamentals/ArraysEx/caminoFactory/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace caminoFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            string input = null;
+            int bestCount = 0;
+            int bestSum = 0;
+            int bestBeginIndex = 0;
+            string bestSequence = "";
+            int counter = 0;
+            int bestCounter = 0;
+
+            while ((input = Console.ReadLine()) != "Clone them")
+            {
+                string sequence = input.Replace("!", "");
+                string[] dnaParts = sequence.Split("0", StringSplitOptions.RemoveEmptyEntries);
+                int count = 0;
+                int sum = 0;
+                string bestSubsequence = "";
+                counter++;
+
+                foreach (string dnaPart in dnaParts)
+                {
+                    if (dnaPart.Length > count)
+                    {
+                        count = dnaPart.Length;
+                        bestSubsequence = dnaPart;
+                    }
+                    sum += dnaPart.Length;
+                }
+                int beginIndex = sequence.IndexOf(bestSubsequence);
+
+                if (count > bestCount ||
+                    count == bestCount && beginIndex < bestBeginIndex||
+                    count == bestCount && beginIndex == bestBeginIndex && sum>bestSum||
+                    counter == 1)
+                {
+                    bestCount = count;
+                    bestSequence = sequence;
+                    bestBeginIndex = beginIndex;
+                    bestSum = sum;
+                    bestCounter = counter;
+                }
+                char[] arr1 = bestSequence.ToCharArray();
+            }
+
+            char[] arr1 = bestSequence.ToCharArray();
+
+            Console.WriteLine($"Best DNA sample {bestCounter} with sum: {bestSum}.");
+            Console.WriteLine(string.Join(" ", arr1));
+        }
+        
+    }
+}
---
+++ Fundamentals/ArraysEx/commonElements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace commonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] arr1 = Console.ReadLine().Split();
+            string[] arr2 = Console.ReadLine().Split();
+            string result = "";
+
+            foreach ( var word1 in arr2)
+            {
+                foreach (var word2 in arr1)
+                {
+                    if (word1 == word2)
+                    {
+                        result += word1 + " ";
+                    }
+                }
+            }
+            Console.WriteLine(result);
+
+            //for (int i = 0; i < arr2.Length; i++)
+            //{
+            //    for (int index = 0; index < arr1.Length; index++)
+            //    {
+            //        if (arr2[i] == arr1[index])
+            //        {
+            //            result = arr2[i] + " ";
+            //        }
+            //    }
+            //}
+           // Console.WriteLine(result);
+
+        }
+    }
+
+}
+    
