ce96d6decf88a2339f7049b05b98b2416f7a51f7
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 18:32:35 +0200

    Add files via upload

---
+++ Fundamentals/ArraysLab/condenseArrayToNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace condenseArrayToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            
+            while (numbers.Length > 1)
+            {
+                int[] newNumbers = new int[numbers.Length - 1];
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    newNumbers[i] = numbers[i] + numbers[i + 1];
+                }
+                numbers = newNumbers;
+            }
+            Console.WriteLine(numbers[0]);
+
+            }
+
+        }
+    }
+
---
+++ Fundamentals/ArraysLab/equalArrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace equalArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] arr2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                sum += arr1[i];
+
+                if (arr1[i] != arr2[i])
+             {
+                Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                return;
+             }
+
+            }
+            sum = arr1.Sum();
+
+        Console.WriteLine($"Arrays are identical. Sum: {sum}");
+
+        }
+
+    }
+}
+ 
