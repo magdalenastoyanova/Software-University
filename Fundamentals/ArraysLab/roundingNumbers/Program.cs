88cc764fb15124cfbb216b8536c0224f11738c1f
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 18:27:50 +0200

    Add files via upload

---
+++ Fundamentals/ArraysLab/roundingNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace roundingNumbers
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            int[] roundednums = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                roundednums[i] = (int)Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+               
+                Console.WriteLine($"{numbers[i]} => {roundednums[i]}");
+
+            }
+           
+
+        }
+    }
+}
