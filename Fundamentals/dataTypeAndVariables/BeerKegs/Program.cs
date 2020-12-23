c38b6e4a3ed9bdafaf2447314e27bb8bea1ced0c
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 18:26:09 +0200

    Add files via upload

---
+++ Fundamentals/dataTypeAndVariables/BeerKegs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numKegs = int.Parse(Console.ReadLine());
+            double bestvolume = 0;
+            string bestModel = string.Empty;
+  ;            for (int i = 1; i <= numKegs; i++)
+            {
+                string model = Console.ReadLine();
+                double r = double.Parse(Console.ReadLine());
+                int h = int.Parse(Console.ReadLine());
+               double volume = Math.PI * r * r * h;
+                if (volume > bestvolume)
+                {
+                    bestvolume = volume;
+                    bestModel = model;
+                }
+                
+            }
+            Console.WriteLine(bestModel);
+        }
+    }
+}
---
+++ Fundamentals/dataTypeAndVariables/dataTpeAndVariabless/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace dataTpeAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+            int sum = (num1 + num2) / num3 * num4;
+            Console.WriteLine(sum);
+
+        }
+    }
+}
