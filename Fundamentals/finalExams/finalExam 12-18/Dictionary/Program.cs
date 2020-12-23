de41e6947a673cf320be416c0a81aedc1d622c6b
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 20:18:45 +0200

    Add files via upload

---
+++ Fundamentals/finalExams/finalExam 12-18/Dictionary/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, List<string>> dic = new SortedDictionary<string, List<string>>();
+            string[] input = Console.ReadLine().Split(new[] { " | " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string[] outputOrder = Console.ReadLine().Split(new[] { " | " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string outputCmd = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] toInp = input[i].Split(new[] { ": " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                if (!dic.ContainsKey(toInp[0]))
+                {
+                    dic.Add(toInp[0], new List<string> { toInp[1] });
+                }
+                else
+                {
+                    dic[toInp[0]].Add(toInp[1]);
+                }
+            }
+            dic.Values.OrderByDescending(t => t.Count());
+
+            if (outputCmd == "List")
+            {
+
+                foreach (var n in dic)
+                {
+                    Console.Write(n.Key + " ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                for (int i = 0; i < outputOrder.Length; i++)
+                {
+                    if (dic.ContainsKey(outputOrder[i]))
+                    {
+                        dic.Values.OrderByDescending(t => t.Count());
+                        foreach (var n in dic)
+                        {
+                            n.Value.Sort((a, b) => b.Length.CompareTo(a.Length));
+                            //n.Value.Reverse();
+                            Console.WriteLine(n.Key);
+                            foreach (var k in n.Value)
+                            {
+                                Console.WriteLine($" -{k}");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Fundamentals/finalExams/finalExam 12-18/decipheringBezRegex/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace decipheringBezRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] comand = Console.ReadLine().Split(" ");
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!((input[i] >= 100 && input[i] <= 125) || input[i] == 35))
+                {
+                    Console.WriteLine("This is not the book you are looking for.");
+                    return;
+                }
+                count++;
+            }
+            StringBuilder result = new StringBuilder();
+            for (int j = 0; j < input.Length; j++)
+            {
+                result.Append((char)((int)input[j] - 3));
+            }
+            result = result.Replace(comand[0], comand[1]);
+            Console.WriteLine(result);
+        }
+    }
+}
