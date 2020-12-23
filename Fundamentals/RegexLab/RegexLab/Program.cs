3988934a50da94eb72a34cbb8d28143835433aa0
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 20:05:12 +0200

    Add files via upload

---
+++ Fundamentals/RegexLab/RegexLab/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string names = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+           
+            Regex regex = new Regex(pattern);
+
+            MatchCollection validNames = regex.Matches(names);
+
+            foreach (Match name in validNames)
+            {
+                Console.Write($"{name.Value}" + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
