bdddbb260b7d60513c5f9947de97dfa66d95f224
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 19:24:30 +0200

    Add files via upload

---
+++ Fundamentals/listsLab/removeNegativeAndReverse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace removeNegativeAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> positives = new List<int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] > 0)
+                {
+                    positives.Add(numbers[i]);
+                }
+            }
+            positives.Reverse();
+
+            if (positives.Count == 0)
+            {
+                Console.WriteLine("empty");
+                return;
+            }
+            Console.WriteLine(string.Join(" ", positives));
+         }
+    }
+}
