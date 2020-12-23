7f19d2a55264350ebf9e3e97b4a46165701d5d0c
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 19:55:33 +0200

    Add files via upload

---
+++ Fundamentals/textProcessingLab/textFilter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace textFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string text = Console.ReadLine();
+
+         text = text.Replace(word, new string('*', word.Length));
+
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Fundamentals/textProcessingLab/textProcessingLab/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace textProcessingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = null;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                var reversedString = input.ToArray().Reverse();
+
+                Console.WriteLine($"{input} = {string.Join("", reversedString)}");
+            }
+        }
+    }
+}
