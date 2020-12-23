6792468189c6c3578c91a02973a9f21781af381d
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 20:36:41 +0200

    Add files via upload

---
+++ Fundamentals/finalExams/finalExam24-07-19/02. Song Encryption/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02._Song_Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([A-Z][a-z\ \']+):([A-Z ]+)";
+            var enterCommand = Console.ReadLine();
+            var builder = new StringBuilder();
+            while (enterCommand != "end")
+            {
+                Match match = Regex.Match(enterCommand, pattern);
+                var splitText = enterCommand.Split(":");
+                if (match.Groups[1].ToString() == splitText[0] && match.Groups[2].ToString() == splitText[1])
+                {
+                    foreach (char item in enterCommand)
+                    {
+                        int newItemValue = item + splitText[0].Length;
+                        if (item == 58)
+                        {
+                            newItemValue = 64;
+                        }
+                        else if (item == 32 || item == 39)
+                        {
+                            newItemValue = item;
+                        }
+                        else if (newItemValue > 90 && item > 64 && item < 91)
+                        {
+                            int difference = newItemValue - 90;
+                            newItemValue = 64 + difference;
+                        }
+                        else if (newItemValue > 122 && item > 96 && item < 123)
+                        {
+                            int difference = newItemValue - 122;
+                            newItemValue = 96 + difference;
+                        }
+                        builder.Append(char.ConvertFromUtf32(newItemValue));
+                    }
+                    Console.WriteLine("Successful encryption: " + builder);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+                enterCommand = Console.ReadLine();
+                builder = new StringBuilder();
+            }
+        }
+    }
+}
---
+++ Fundamentals/finalExams/finalExam24-07-19/03. The Isle of Man TT Race/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _03._The_Isle_of_Man_TT_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([#$%*&])(?<name>[A-z]+)(\1)=(?<lenght>\d+)!!(?<code>.+)";
+            string input = Console.ReadLine();
+            while (true)
+            {
+                Regex order = new Regex(pattern);
+                if (order.IsMatch(input))
+                {
+                    string name = order.Match(input).Groups["name"].Value;
+                    int lenght = int.Parse(order.Match(input).Groups["lenght"].Value);
+                    string code = order.Match(input).Groups["code"].Value;
+                    if (lenght == code.Length)
+                    {
+                        StringBuilder codee = new StringBuilder();
+                        for (int i = 0; i < code.Length; i++)
+                        {
+                            char symbol = (char)(code[i] + lenght);
+                            codee.Append(symbol);
+                        }
+                        Console.WriteLine($"Coordinates found! {name} -> {codee}");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing found!");
+                    }
+
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Nothing found!");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
