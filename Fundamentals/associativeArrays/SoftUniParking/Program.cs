0e62e1389a6d5d81db821b2c48a049706ff0ab4a
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 19:39:40 +0200

    Add files via upload

---
+++ Fundamentals/associativeArrays/SoftUniParking/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUniParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numCommands = int.Parse(Console.ReadLine());
+
+            Dictionary<string, string> userLicense = new Dictionary<string, string>();
+
+            for (int i = 0; i < numCommands; i++)
+            {
+                var input = Console.ReadLine().Split(' ');
+                var command = input[0];
+                var username = input[1];
+
+                if (input[0] == "register")
+                {
+                    var license = input[2];
+
+                    if (!userLicense.ContainsKey(username))
+                    {
+                        if (!userLicense.ContainsKey(license))
+                        {
+                            userLicense[username] = license;
+                            Console.WriteLine($"{username} registered {license} successfully");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {license}");
+                    }
+
+                }
+                else
+                {
+                    if (!userLicense.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                    else
+                    {
+                        userLicense.Remove(username);
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, userLicense.Select(x => $"{x.Key} => {x.Value}")));
+        }
+    }
+}
---
+++ Fundamentals/associativeArrays/StudentAcademy/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> listOfGrades = new Dictionary<string, List<double>>();
+            string studentName;
+            double studentGrade = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                studentName = Console.ReadLine();
+                studentGrade = double.Parse(Console.ReadLine());
+
+                if (!listOfGrades.ContainsKey(studentName))
+                {
+
+                    listOfGrades.Add(studentName, new List<double>());
+
+                }
+
+                listOfGrades[studentName].Add(studentGrade);
+
+            }
+            foreach (var eachStudent in listOfGrades.OrderByDescending(x => x.Value.Average()))
+            {
+
+                if (eachStudent.Value.Average() >= 4.5)
+                {
+                    Console.WriteLine($"{eachStudent.Key} -> {eachStudent.Value.Average():f2}");
+                }
+            }
+        }
+
+    }
+
+ }
+    
+
