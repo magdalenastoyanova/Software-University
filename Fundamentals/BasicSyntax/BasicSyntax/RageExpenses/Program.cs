8300869635cad1b33964d4f2c76acbc0283ae957
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 18:10:05 +0200

    Add files via upload

---
+++ Fundamentals/BasicSyntax/BasicSyntax/RageExpenses/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RageExpenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            double trashedHeadset = Math.Floor(lostGames / 2.0);
+            double trashedMouse = Math.Floor(lostGames / 3.0);
+            double trashedkeyboard = Math.Floor(lostGames / 6.0);
+            double trashedDisplay = Math.Floor(lostGames / 12.0);
+
+            double totalExpenses = (headsetPrice * trashedHeadset) + (mousePrice * trashedMouse) + (keyboardPrice * trashedkeyboard) + (displayPrice * trashedDisplay);
+
+
+            Console.WriteLine($"Rage expenses: {totalExpenses:F2} lv.");
+        }
+    }
+}
---
+++ Fundamentals/BasicSyntax/BasicSyntax/strongNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace strongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int tempNum = num;
+            int totalFactorialSum = 0;
+            while (tempNum > 0) 
+            {
+                int digit = tempNum % 10;
+                tempNum /= 10;
+                int currentFactorialSum = 1;
+                for (int i = 1; i < digit; i++)
+                {
+                   currentFactorialSum *= i;
+                }
+                totalFactorialSum += currentFactorialSum;
+            }
+            if (num == totalFactorialSum)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
