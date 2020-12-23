f4a758516d49aa013e0ffe0fb3c21371c49b3d51
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 18:16:44 +0200

    Add files via upload

---
+++ Fundamentals/dataTypeAndVariables/TripletsOfLatinLetters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TripletsOfLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char endLetter = (char)(97 + n);
+            for (char a = 'a'; a < endLetter; a++)
+            {
+                for (char b = 'a'; b < endLetter; b++)
+                {
+                    for (char c = 'a'; c < endLetter; c++)
+                    {
+                        Console.WriteLine($"{a}{b}{c}");
+                    }
+                }
+               
+            }
+            
+        }
+    }
+}
---
+++ Fundamentals/dataTypeAndVariables/waterOverflow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace waterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfLines = int.Parse(Console.ReadLine());
+            int capacity = 0;
+            int maxCapacity = 255;
+
+            for (int i = 0; i < numOfLines; i++)
+            {
+                int liters = int.Parse(Console.ReadLine());
+                if (capacity + liters <= maxCapacity)
+                {
+                    capacity += liters;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+
+            }
+            Console.WriteLine(capacity);
+        }
+    }
+}
