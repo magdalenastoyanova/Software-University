e7603a895587f498412e56f6298a176ef9b5ce54
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 18:29:17 +0200

    Add files via upload

---
+++ Fundamentals/ArraysLab/evenAndOddSubstraction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace evenAndOddSubstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumOdd = 0;
+            int sumEven = 0;
+            int result = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNums = numbers[i];
+                if (currentNums % 2 == 0)
+                {
+                    sumEven += currentNums; 
+                }
+                else 
+                {
+                    sumOdd += currentNums;
+                }
+                result = sumEven - sumOdd;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Fundamentals/ArraysLab/reverseArrayOfStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace reverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split(' ').ToArray();
+
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                var oldElement = arr[i];
+                arr[i] = arr[arr.Length - i - 1];
+                arr[arr.Length - i - 1] = oldElement;
+            }
+
+            Console.Write(string.Join((" "), arr));
+        }
+    }
+}
