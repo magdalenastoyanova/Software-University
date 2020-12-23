2be145aa250686df5e1fd5f6892c923b5ec37eb2
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 20:11:46 +0200

    Add files via upload

---
+++ Fundamentals/RegexEx/03.SoftUniBarIncome/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03.SoftUniBarIncome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^%(?<customer>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.]*?(?<price>[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)\$";
+            double totalIncome = 0;
+            string line = Console.ReadLine();
+
+            while (line != "end of shift")
+            {
+                if (Regex.IsMatch(line, pattern))
+                {
+                    Match match = Regex.Match(line, pattern);
+                    var customer = match.Groups["customer"].Value;
+                    string product = match.Groups["product"].Value;
+                    int count = int.Parse(match.Groups["count"].Value);
+                    double price = double.Parse(match.Groups["price"].Value);
+                    double money = price * count;
+                    Console.WriteLine($"{customer}: {product} - {money:F2}");
+                    totalIncome += money;
+                }
+                line = Console.ReadLine();
+
+            }
+           
+            Console.WriteLine($"Total income: {totalIncome:F2}");
+        }
+    }
+}
---
+++ Fundamentals/RegexEx/06.ExtractEmails/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06.ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string pattern = @"(^|(?<=\s))(([a-zA-Z0-9]+)([\.\-_]?)([A-Za-z0-9]+)(@)([a-zA-Z]+([\.\-_][A-Za-z]+)+))(\b|(?=\s))";
+
+            Regex emails = new Regex(pattern);
+
+            MatchCollection collection = Regex.Matches(text, pattern);
+
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
