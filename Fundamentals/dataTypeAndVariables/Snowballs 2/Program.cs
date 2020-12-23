1fa1a24b3e3bf48f5af1d753945dbeba46352985
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 18:18:18 +0200

    Add files via upload

---
+++ Fundamentals/dataTypeAndVariables/Snowballs 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+namespace Snowballs_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+                int number = int.Parse(Console.ReadLine());
+                int counter = 0;
+                double maxValue = double.MinValue;
+                int endSnowball = 0;
+                int endSnowballTime = 0;
+                int endSnowballQuality = 0;
+
+                while (counter < number)
+                {
+                    int snowball = int.Parse(Console.ReadLine());
+                    int snowballTime = int.Parse(Console.ReadLine());
+                    int snowballQuality = int.Parse(Console.ReadLine());
+                   BigInteger formula = (BigInteger).(snowball / snowballTime, snowballQuality);
+
+                    if (formula > maxValue)
+                    {
+                        maxValue = formula;
+                        endSnowball = snowball;
+                        endSnowballTime = snowballTime;
+                        endSnowballQuality = snowballQuality;
+                    }
+
+                    counter++;
+                }
+                Console.WriteLine($"{endSnowball} : {endSnowballTime} = {maxValue} ({endSnowballQuality})");
+            }
+        }
+    }
---
+++ Fundamentals/dataTypeAndVariables/spiceMustFlow/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace spiceMustFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int decrease = 10;
+            int totalAmount = 0;
+            int spiceForTheCrew = 26;
+            int days = 0;
+           
+            while (startingYield >= 100)
+            {
+                days++;
+                int yieldSpiceForTheDay = startingYield;
+                if (yieldSpiceForTheDay > spiceForTheCrew)
+                {
+                    yieldSpiceForTheDay -= spiceForTheCrew;
+                }
+                startingYield -= decrease;
+                totalAmount += yieldSpiceForTheDay;
+                if (startingYield < 100)
+                {
+                    break;
+                }
+            }
+            if (totalAmount > spiceForTheCrew)
+            {
+                totalAmount -= spiceForTheCrew;
+            }
+            Console.WriteLine($"{days}");
+            Console.WriteLine($"{totalAmount}");
+        }
+    }
+}
