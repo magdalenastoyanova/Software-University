db9120e1f4666473d190af031546a7955f961a9c
magdalenastoyanova <73058577+magdalenastoyanova@users.noreply.github.com>
2020-12-23 19:58:56 +0200

    Add files via upload

---
+++ Fundamentals/textProcessingEx/validateUsername/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace textProcessingEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(", ", StringSplitOptions.None).ToArray();
+
+            foreach (var username in words)
+            {
+                if (ValidateUsername(username))
+                {
+                    Console.WriteLine(username);
+                }
+            }
+        }
+        private static bool ValidateUsername(string username) 
+        {
+            
+            if (username.Length < 3 || username.Length > 16)
+            {
+                return false;
+            }
+
+            bool isValid = true;
+
+            for (int i = 0; i < username.Length; i++)
+            {
+                char currentCh = username[i];
+
+                if (!(Char.IsLetterOrDigit(currentCh) || currentCh == '-' || currentCh == '_'))
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+            return isValid;
+        }
+    }
+    
+}
